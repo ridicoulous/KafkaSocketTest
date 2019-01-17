ec4eabeb13e47fc102a561d9435bfeaabfa35b28
msg_kurt <artemkuryanov@gmail.com>
2019-01-17 13:25:08 +0200

    Add project files.

---
+++ KafkaSocketTest/Program.cs
@@ -0,0 +1,72 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+namespace KafkaSocketTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conf = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            Action<DeliveryReportResult<Null, string>> handler = r =>
+                Console.WriteLine(!r.Error.IsError
+                    ? $"Delivered message to {r.TopicPartitionOffset}"
+                    : $"Delivery Error: {r.Error.Reason}");
+            using (var p = new Producer<Null, string>(conf))
+            {                
+                for (int i = 0; i < 100; ++i)
+                {
+                    p.BeginProduce("my-topic", new Message<Null, string> { Value = i.ToString() }, handler);
+                }
+                // wait for up to 10 seconds for any inflight messages to be delivered.
+                p.Flush(TimeSpan.FromSeconds(10));
+            }
+            Consume();
+            Console.WriteLine("Consumed... PAK, pls");
+            Console.ReadLine();
+
+        }
+        public static void Consume()
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // earliest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetResetType.Earliest
+            };
+
+            using (var c = new Consumer<Ignore, string>(conf))
+            {
+                c.Subscribe("my-topic");
+
+                bool consuming = true;
+                // The client will automatically recover from non-fatal errors. You typically
+                // don't need to take any action unless an error is marked as fatal.
+                c.OnError += (_, e) => consuming = !e.IsFatal;
+
+                while (consuming)
+                {
+                    try
+                    {
+                        var cr = c.Consume();
+                        Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+
+                // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                c.Close();
+            }
+        }
+    }
+}
