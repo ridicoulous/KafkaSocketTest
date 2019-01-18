5b33a1cece38da6e6bf3340ba6591e03cf30b943
msg_kurt <artemkuryanov@gmail.com>
2019-01-18 17:17:58 +0200

    adn push

--- KafkaSocketTest/Program.cs
+++ KafkaSocketTest/Program.cs
@@ -12,52 +12,52 @@ namespace KafkaSocketTest
             Console.WriteLine("Starting...");    
             var t = new SocketListener.SocketListener();
             t.Subcribe("WAVESBTC");
-            Consume();
+          //  Consume();
             Console.WriteLine("Consumed... PAK, pls");
-          //  Console.ReadLine();
+            Console.ReadLine();
 
         }
-        public static void Consume()
-        {
-            var conf = new ConsumerConfig
-            {
-                GroupId = "socket-consumer-group",
-                BootstrapServers = "localhost:9092",
-                // Note: The AutoOffsetReset property determines the start offset in the event
-                // there are not yet any committed offsets for the consumer group for the
-                // topic/partitions of interest. By default, offsets are committed
-                // automatically, so in this example, consumption will only start from the
-                // earliest message in the topic 'my-topic' the first time you run the program.
-                AutoOffsetReset = AutoOffsetResetType.Earliest
-            };
+        //public static void Consume()
+        //{
+        //    var conf = new ConsumerConfig
+        //    {
+        //        GroupId = "socket-consumer-group",
+        //        BootstrapServers = "localhost:9092",
+        //        // Note: The AutoOffsetReset property determines the start offset in the event
+        //        // there are not yet any committed offsets for the consumer group for the
+        //        // topic/partitions of interest. By default, offsets are committed
+        //        // automatically, so in this example, consumption will only start from the
+        //        // earliest message in the topic 'my-topic' the first time you run the program.
+        //        AutoOffsetReset = AutoOffsetResetType.Earliest
+        //    };
 
-            using (var c = new Consumer<Ignore, string>(conf))
-            {
-                Console.WriteLine("Consumer started");
-                c.Subscribe("socket");
+        //    using (var c = new Consumer<Ignore, string>(conf))
+        //    {
+        //        Console.WriteLine("Consumer started");
+        //        c.Subscribe("socket");
 
-                bool consuming = true;
-                // The client will automatically recover from non-fatal errors. You typically
-                // don't need to take any action unless an error is marked as fatal.
-                c.OnError += (_, e) => consuming = !e.IsFatal;
+        //        bool consuming = true;
+        //        // The client will automatically recover from non-fatal errors. You typically
+        //        // don't need to take any action unless an error is marked as fatal.
+        //        c.OnError += (_, e) => consuming = !e.IsFatal;
 
-                while (consuming)
-                {
-                    //if (Console.ReadKey().Key == ConsoleKey.Escape)
-                    //    break;                        
-                    try
-                    {
-                        var cr = c.Consume();
-                        Console.WriteLine($"{cr.Value}");
-                    }
-                    catch (ConsumeException e)
-                    {
-                        Console.WriteLine($"Error occured: {e.Error.Reason}");
-                    }
-                }
-                // Ensure the consumer leaves the group cleanly and final offsets are committed.
-                c.Close();
-            }
-        }
+        //        while (consuming)
+        //        {
+        //            //if (Console.ReadKey().Key == ConsoleKey.Escape)
+        //            //    break;                        
+        //            try
+        //            {
+        //                var cr = c.Consume();
+        //                Console.WriteLine($"{cr.Value}");
+        //            }
+        //            catch (ConsumeException e)
+        //            {
+        //                Console.WriteLine($"Error occured: {e.Error.Reason}");
+        //            }
+        //        }
+        //        // Ensure the consumer leaves the group cleanly and final offsets are committed.
+        //        c.Close();
+        //    }
+        //}
     }
 }
--- KafkaSocketTest/SocketListener/SocketListener.cs
+++ KafkaSocketTest/SocketListener/SocketListener.cs
@@ -13,17 +13,20 @@ namespace KafkaSocketTest.SocketListener
         BinanceSocketClient _socketClient;
         BinanceClient _apiClient;
         Producer<Null, string> _producer = new Producer<Null, string>(new ProducerConfig { BootstrapServers = "localhost:9092" });
-        Action<DeliveryReportResult<Null, string>> handler = r =>
-         Console.WriteLine(!r.Error.IsError
-             ? $"Delivered message to {r.TopicPartitionOffset}"
-             : $"Delivery Error: {r.Error.Reason}");
+        //Action<DeliveryReportResult<Null, string>> handler = r => Void(r);
         public SocketListener(string key = "i8esSKM1QA6H7DgyCLPFDf17bPXRsWu7ZxBoD4uothQXwRGpvFkJeuOgRgp7cYNM", string secret = "iKWu94GjIwVLBoPORH0pt1sUuqf3MtJobGrQjdu4033kje43Nhe8tQnSK9pnXXgH", string name = "Test", long channelId = -1001483025408)
         {
             Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
             _apiClient = new BinanceClient(new BinanceClientOptions() { ApiCredentials = new CryptoExchange.Net.Authentication.ApiCredentials(key, secret) });
             _socketClient = new BinanceSocketClient(new BinanceSocketClientOptions() { ApiCredentials = new CryptoExchange.Net.Authentication.ApiCredentials(key, secret) });
         }
-
+        public void Void(DeliveryReportResult<Null, string> r)
+        {
+            if (r.Error.IsError)
+            {
+                Console.WriteLine($"Delivery Error: {r.Error.Reason}");
+            }
+        }
         public void Subcribe(string pair)
         {
             var symbolTtradeSubscription = _socketClient.SubscribeToTradesStream(pair, _ => ProduceEvent(_));
@@ -31,21 +34,29 @@ namespace KafkaSocketTest.SocketListener
                 Console.WriteLine(symbolTtradeSubscription.Data.Id);
             else
                 Console.WriteLine(symbolTtradeSubscription.Error.Message);
-        }       
+            symbolTtradeSubscription.Data.ConnectionLost += Data_ConnectionLost;            
+
+        }
+
+        private void Data_ConnectionLost()
+        {
+            Console.WriteLine("Connection lost. Reconnecting...");
+            Subcribe("WAVESBTC");
+        }
+
         public void ProduceEvent(BinanceStreamTrade e)
         {
             try
-            {
-                Console.WriteLine("catched");
+            {                
                 _producer.BeginProduce("socket", new Message<Null, string>
                 {
                     Value = JsonConvert.SerializeObject(e)
-                }, handler);
-                _producer.Flush(TimeSpan.FromSeconds(10));
+                }, Void);
+                _producer.Flush(TimeSpan.FromSeconds(42));
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.ToString());                
+                Console.WriteLine(ex.ToString());
             }
         }
 
