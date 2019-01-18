9c47e3500a882ca58d8a78a2ec1135b78fb2c8fa
msg_kurt <artemkuryanov@gmail.com>
2019-01-18 18:07:45 +0200

    42

--- KafkaSocketTest/SocketListener/SocketListener.cs
+++ KafkaSocketTest/SocketListener/SocketListener.cs
@@ -1,6 +1,7 @@
 ﻿using Binance.Net;
 using Binance.Net.Objects;
 using Confluent.Kafka;
+using CryptoExchange.Net.Converters;
 using Newtonsoft.Json;
 using System;
 using System.Globalization;
@@ -10,7 +11,7 @@ namespace KafkaSocketTest.SocketListener
 {
     public class SocketListener
     {
-        
+
         BinanceSocketClient _socketClient;
         BinanceClient _apiClient;
         Producer<Null, string> _producer = new Producer<Null, string>(new ProducerConfig { BootstrapServers = "localhost:9092" });
@@ -30,12 +31,12 @@ namespace KafkaSocketTest.SocketListener
         }
         public void Subcribe(string pair)
         {
-            var symbolTtradeSubscription = _socketClient.SubscribeToTradesStream(pair, _ => ProduceEvent(_));            
+            var symbolTtradeSubscription = _socketClient.SubscribeToTradesStream(pair, _ => ProduceEvent(_));
             if (symbolTtradeSubscription.Success)
                 Console.WriteLine(symbolTtradeSubscription.Data.Id);
             else
                 Console.WriteLine(symbolTtradeSubscription.Error.Message);
-            symbolTtradeSubscription.Data.ConnectionLost += Data_ConnectionLost;            
+            symbolTtradeSubscription.Data.ConnectionLost += Data_ConnectionLost;
 
         }
 
@@ -44,14 +45,45 @@ namespace KafkaSocketTest.SocketListener
             //Console.WriteLine("Connection lost. Reconnecting...");
             Subcribe("WAVESBTC");
         }
-
+        public class BinanceStreamClick
+        {
+            public BinanceStreamClick(BinanceStreamTrade t)
+            {
+                Symbol = t.Symbol;
+                TradeId = t.TradeId;
+                Price = t.Price;
+                Quantity = t.Quantity;
+                BuyerOrderId = t.BuyerOrderId;
+                SellerOrderId = t.SellerOrderId;
+                TradeTime = t.TradeTime;
+                BuyerIsMaker = t.BuyerIsMaker ? 1 : 0;
+            }
+            [JsonProperty("s")]
+            public string Symbol { get; set; }
+            [JsonProperty("t")]
+            public long TradeId { get; set; }
+            [JsonProperty("p")]
+            public decimal Price { get; set; }
+            [JsonProperty("q")]
+            public decimal Quantity { get; set; }
+            [JsonProperty("b")]
+            public long BuyerOrderId { get; set; }
+            [JsonProperty("a")]
+            public long SellerOrderId { get; set; }
+            [JsonConverter(typeof(TimestampConverter))]
+            [JsonProperty("T")]
+            public DateTime TradeTime { get; set; }
+            [JsonProperty("m")]
+            public int BuyerIsMaker { get; set; }           
+           
+        }
         public void ProduceEvent(BinanceStreamTrade e)
         {
             try
-            {                
+            {
                 _producer.BeginProduce("socket", new Message<Null, string>
                 {
-                    Value = JsonConvert.SerializeObject(e)
+                    Value = JsonConvert.SerializeObject(new BinanceStreamClick(e) )
                 }, Void);
                 _producer.Flush(TimeSpan.FromSeconds(42));
             }
